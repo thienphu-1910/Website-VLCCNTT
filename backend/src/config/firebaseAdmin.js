423273f4ad137ef34d16f9d3a23414425c3ae8b6
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2025-12-01 00:02:49 +0700

    choir(mockData): add seed sql nma ignore roi nen ke di

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,16 +1,17 @@
-import admin from 'firebase-admin';
-import dotenv from 'dotenv';
+const admin = require('firebase-admin');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS) : undefined;
+const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS ? process.env.GOOGLE_APPLICATION_CREDENTIALS : undefined;
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     projectId: process.env.FIREBASE_PROJECT_ID,
 });
 
+
 const db = admin.firestore();
 const auth = admin.auth();
 
-export { db, auth };
+module.exports = { admin, db, auth };
