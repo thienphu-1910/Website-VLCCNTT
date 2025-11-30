091e7d7e98338f13d2f0a7e671bffd09214e4f0d
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2025-11-30 23:13:01 +0700

    chore: invoke showToast

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
 import { useForm } from "react-hook-form";
+import { showToast } from "react-toastify"
 
 const FormHeader = () => {
   return (
