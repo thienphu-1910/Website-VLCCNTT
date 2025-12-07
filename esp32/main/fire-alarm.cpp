f99e57cbdb52957dcb49a953e1021aaf4cddd4e8
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2025-12-07 15:36:18 +0700

    feat(esp32): add fire alarm logic - no wifi logic yet

--- esp32/main/fire-alarm.cpp
+++ esp32/main/fire-alarm.cpp
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <string>
+#include <iostream>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "nvs_flash.h"
-#include "_mqtt/_mqtt.hpp"
-#include  "smoke_sensor/smoke-sensor.hpp"
+#include "esp_netif.h"
+#include "_mqtt.hpp"
+#include  "smoke-sensor.hpp"
+#include "init.hpp"
 
 
-void app_main(void)
-{
-    
+void app_main(void) { 
+    bool init_success = init_esp_setup();
+
+    if (init_success == false) {
+        std::cout << "ESP Setup Initialization Failed!" << std::endl;
+        return;
+    }
 
     std::string mqtt_uri = "";
     SmokeSensor smokeSensor(34);
     MQTTHandler mqttHandler(mqtt_uri);
+
+    mqttHandler.start();
+
+    while (true) {
+        smokeSensor.setSmokeLevel();
+        int smokeLevel = smokeSensor.getSmokeLevel();
+        std::string topic = "home/fire/smoke";
+        std::string message = "Smoke Level: " + std::to_string(smokeLevel) + " PPM";
+        int qos = 1;
+        int retain = 0;
+        mqttHandler.publish(topic, message, qos, retain);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
 }
