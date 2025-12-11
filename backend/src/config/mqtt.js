879fa6b0b51a36633d435b7eaba8039378276ad1
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2025-12-11 17:22:06 +0700

    feat(backend): add mqtt logic

--- backend/src/config/mqtt.js
+++ backend/src/config/mqtt.js
@@ -3,26 +3,47 @@ const { db } = require('./firebaseAdmin.js');
 
 const brokerUrl = process.env.MQTT_BROKER_URL || 'mqtt://localhost:1883';
 const topic = process.env.MQTT_TOPIC || 'sensors/fire';
+const clientId = process.env.MQTT_CLIENT_ID || 'mqtt_client';
+const username = process.env.MQTT_USERNAME || '';
+const password = process.env.MQTT_PASSWD || '';
 
-const client = mqtt.connect(brokerUrl);
+const option = {
+    connectTimeout: 4000,
+
+    clientId: clientId,
+    username: username,
+    password: password
+};
+
+const client = mqtt.connect(brokerUrl, option);
 
 client.on('connect', () => {
-    console.log("Connected to MQTT broker at ", brokerUrl);
-
-    client.subscribe(topic, (err) => {
+    console.log('Connected to MQTT broker');
+    client.subscribe(topic, (err)=> {
         if (err === null) {
-            console.log(`Subscribed to topic: ${topic}`);
+            console.log('Subscribe successfully');
+        } else {
+            console.error('Subscribe failed: ', err);
         }
     });
 });
 
-client.on('message', (topic, message) => {
-    console.log(`Received message on topic ${topic}: ${message.toString()}`);
-    const payload = JSON.parse(message.toString());
-
-    // Write to Firestore database
-    db.collection('sensorData').add({
-        deviceId: payload.deviceId,
-        smokeLevel: payload.smokeLevel,
-    });
+client.on('message', async (topic, message) => {
+    console.log("Message received on topic ", topic, ": ", message.toString());
+    try {
+        const data = JSON.parse(message.toString());
+        const device_id = data['deviceID'];
+        const current_time = new Date();
+        await db.collection('sensor_data').doc(device_id).set({
+            device_id: device_id,
+            temperature: data['temperature'],
+            co_percent: data['co_percent'],
+            has_smoke: data['has_smoke'],
+            current_time: toString(current_time)
+        });
+    } catch (error) {
+        console.error("Error handling message: ", error);
+    }
 });
+
+module.exports =  { client }
