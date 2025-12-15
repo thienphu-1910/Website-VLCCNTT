a469dcd8d36a0a054279aea8e9aeca92a78ecbc8
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2025-12-15 17:53:51 +0700

    hehe

--- backend/src/config/mqtt.js
+++ backend/src/config/mqtt.js
@@ -1,8 +1,6 @@
 import mqtt from "mqtt";
-import { db } from "./firebaseAdmin.js";
 
 const brokerUrl = process.env.MQTT_BROKER_URI || '';
-const topic = process.env.MQTT_TOPIC || '';
 const clientId = process.env.MQTT_CLIENT_ID || '';
 const username = process.env.MQTT_USERNAME || '';
 const password = process.env.MQTT_PASSWORD || '';
@@ -11,7 +9,7 @@ const port = process.env.MQTT_PORT || 8883;
 const option = {
     port: port,
     connectTimeout: 4000,
-
+    reconnectPeriod: 1000,
     clientId: clientId,
     username: username,
     password: password
@@ -19,34 +17,13 @@ const option = {
 
 const client = mqtt.connect(brokerUrl, option);
 
-client.on('connect', () => {
-    console.log('Connected to MQTT broker');
-    client.subscribe(topic, (err)=> {
-        if (err === null) {
-            console.log('Subscribe successfully');
-        } else {
-            console.error('Subscribe failed: ', err);
-        }
-    });
+client.on("error", (error) => {
+    console.error("MQTT Error: ", error.message);
+    client.end();
 });
 
-client.on('message', async (topic, message) => {
-    console.log("Message received on topic ", topic, ": ", message.toString());
-    try {
-        const data = JSON.parse(message.toString());
-        const device_id = data['deviceID'];
-        const current_time = new Date();
-        await db.collection('sensor_data').doc(device_id).set({
-            device_id: device_id,
-            temperature: data['temperature'],
-            co_percent: data['co_percent'],
-            has_smoke: data['has_smoke'],
-            flame: data['flame'],
-            current_time: toString(current_time)
-        });
-    } catch (error) {
-        console.error("Error handling message: ", error);
-    }
+client.on("reconnect", () => {
+    console.log("Reconnecting...");
 });
 
 export { client };
