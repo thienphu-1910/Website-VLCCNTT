56ed437e82bffb32ff9f8e67012f27fbac2e4224
Akuseru0509 <nguyenminhkhoi2329@gmail.com>
2025-12-16 10:53:41 +0700

    temp sensor nma chua xong

--- fire_alarm/main/_temperature_sensor/_temperature_sensor.h
+++ fire_alarm/main/_temperature_sensor/_temperature_sensor.h
@@ -1,10 +1,17 @@
 #ifndef _TEMPERATURE_SENSOR_H
 #define _TEMPERATURE_SENSOR_H
 
+#include "esp_adc/adc_oneshot.h"
+
 class TemperatureSensor {
 private:
-
+    adc_oneshot_unit_handle_t _handler;
+    adc_channel_t _pin;
+    float _temperature;
 public:
+    TemperatureSensor(adc_channel_t pin);
+    void start();
+    float getTemperature();
 };
 
 #endif 
